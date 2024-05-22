b565d11b105b880193331bd03bc0a692815033d4
SpartanMafia <danishzain1637@gmail.com>
2024-05-22 11:45:44 +0530

    Update Gpt.py

--- DAXXMUSIC/plugins/tools/Gpt.py
+++ DAXXMUSIC/plugins/tools/Gpt.py
@@ -7,7 +7,7 @@ from config import BOT_USERNAME
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
-@app.on_message(filters.command(["chatgpt","Xd","ask","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
+@app.on_message(filters.command(["chatgpt","Xd","Duggi","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
 async def chat_gpt(bot, message):
     try:
         start_time = time.time()
